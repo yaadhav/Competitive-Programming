d64bbfa5acdf048432c7ed6c3192c72b6ce73a45
unknown <raviyaadhav@gmail.com>
2023-02-17 23:59:27 +0530

    Updated File

---
+++ HR/Problem Solving/Book/1.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int pageCount(int n, int p) {
+    
+    int min=(n/2)-(p/2);
+
+    if( min>p/2 )
+        min=p/2;
+    
+    return min;
+}
+
+int main()
+{
+    int n, p;
+    cin >> n >> p;
+
+    int result=pageCount(n, p);
+
+    cout << result ;
+}
---
+++ HR/Problem Solving/Staircase/1.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void staircase(int n) {
+
+    int c=n-1;    
+    for( int i=1; i<=n; i++)
+    {
+        for( int j=1; j<c; j++)
+            cout << " " ;
+        for( int j=c; j<n; j++)
+            cout << "#" ;
+        cout << endl;
+        c--;
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    staircase(n);
+}
