6737308c7aaa8016c8ba1fed2fa4493202babcb9
unknown <raviyaadhav@gmail.com>
2023-02-27 23:44:24 +0530

    Updated Commit

--- CF/PROBLEM SET/800,900/Night at The Museum/1.cpp
+++ CF/PROBLEM SET/800,900/Night at The Museum/1.cpp
@@ -1,3 +1,5 @@
+// Q : https://codeforces.com/problemset/problem/731/A
+
 #include <bits/stdc++.h>
 using namespace std;
  
@@ -6,30 +8,23 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int t;
-    cin >> t;
+    string s;
+    cin >> s;
 
-    while( t-- )
+    int cmp, count=0;
+    char c='a';
+
+    for( int i=0; i<s.length(); i++)
     {
-        string s;
-        cin >> s;
-
-        int cmp, count=0;
-        char c='a';
-
-        for( int i=0; i<s.length(); i++)
-        {
-            cmp=abs((int)c-(int)s[i]);
-
-            if( cmp>13 )
-                cmp=26-cmp;
-            
-            count+=cmp;
-            c=s[i];
-        }
-
-        cout << count << endl ;
+        cmp=abs((int)c-(int)s[i]);
+        if( cmp>13 )
+            cmp=26-cmp;
+        
+        count+=cmp;
+        c=s[i];
     }
+    
+    cout << count << endl ;
         
     return 0;
 }
