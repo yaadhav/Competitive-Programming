25074cf6bfa9fe64e28ef83525c412c187099b0b
unknown <raviyaadhav@gmail.com>
2023-03-17 00:14:02 +0530

    Updated Commit

--- CF/PROBLEM SET/1100/Kind Anton/1.cpp
+++ CF/PROBLEM SET/1100/Kind Anton/1.cpp
@@ -1,4 +1,4 @@
-// Q : https://codeforces.com/problemset/problem/478/A
+// Q : https://codeforces.com/problemset/problem/1333/B
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -8,18 +8,50 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
-    int temp, sum=0;
-    
-    for( int i=0; i<5; i++)
+    int t;
+    cin >> t;
+    while( t-- )
     {
-        cin >> temp;
-        sum+=temp;
+        int n;
+        cin >> n;
+
+        vector<int> a, b;
+        int temp, neg=0, pos=0;
+
+        for( int i=0; i<n; i++)
+        {
+            cin >> temp;
+            a.push_back(temp);
+
+            if( temp==1 )
+                pos++;
+            else if( temp==-1 )
+                neg++;
+        }
+
+        for( int i=0; i<n; i++)
+        {
+            cin >> temp;
+            b.push_back(temp);
+        }
+
+        string ans = a[0]==b[0] ? "YES":"NO" ;
+
+        for( int i=n-1; i>=0 && ans=="YES"; i-- )
+        {
+            if( a[i]==1 )
+                pos--;
+            else if( a[i]==-1 )
+                neg--;
+
+            if( b[i]<a[i] && neg==0 )
+                ans="NO"; 
+            else if( b[i]>a[i] && pos==0 )
+                ans="NO";   
+        }
+
+        cout << ans << endl;
     }
-
-    if( sum%5==0 && sum>0 )
-        cout << sum/5 ;
-    else
-        cout << -1 ;   
-
+    
     return 0;
 }
