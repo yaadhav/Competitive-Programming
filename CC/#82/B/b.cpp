4c86ec9c1c3996674060298d69872e6cfa574f11
unknown <raviyaadhav@gmail.com>
2023-03-24 00:01:42 +0530

    Updated File

---
+++ CC/#82/B/b.cpp
@@ -0,0 +1,26 @@
+// Q : https://www.codechef.com/START81D/problems/REACH_HOME
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t ;
+
+    while(t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        string ans="NO";
+
+        if( x*5>=y )
+            ans="YES";
+        
+        cout << ans << endl; 
+    } 
+    
+    return 0;
+}
+
