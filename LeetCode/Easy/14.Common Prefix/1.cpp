6ee5f42732e90d52cd2443891419e444fc46e587
unknown <raviyaadhav@gmail.com>
2023-05-16 00:37:33 +0530

    Updated Commit

---
+++ LeetCode/Easy/14.Common Prefix/1.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+
+        sort(strs.begin(), strs.end());
+
+        string str1=strs[0], str2=strs[strs.size()-1], common="";
+
+        for( int i=0; i<str1.length(); i++)
+        {
+            if( str1[i]==str2[i] )
+                common+=str1[i];
+            else
+                break;
+        }
+
+        return common;
+        
+    }
+};
+
+int main()
+{
+    int n=0;
+    //cin >> n;
+
+    vector<string> strs={"fliwer","fliw","flight","flite","flirt"};
+    string temp; 
+
+    for( int i=0; i<n; i++)
+    {
+        cin >> temp;
+        strs.push_back(temp);
+    }
+
+    Solution s;
+    string result=s.longestCommonPrefix(strs);
+    cout << result;
+}
