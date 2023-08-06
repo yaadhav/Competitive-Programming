0f006ec4f6b9a3173f0ebf1b859f7656416bae86
unknown <raviyaadhav@gmail.com>
2023-08-06 20:10:17 +0530

    Updated Commit

--- LeetCode/Easy/70.Climbing Stairs/70.cpp
+++ LeetCode/Easy/70.Climbing Stairs/70.cpp
@@ -3,31 +3,21 @@ using namespace std;
 
 class Solution {
 public:
-    unsigned long long int fact( int a, int b )
-    {
-        unsigned long long int res=1;
-        for( int i=a; i>b; i--)
-            res*=i;
-        return res;
-    }
 
     int climbStairs(int n) {
-        unsigned long long int ans=0, a, b;
-        for( int i=n, j=0; i>=(n+1)/2; i--,j++)
+        
+        unsigned long long int first=0, next=1, curr;
+        for( int i=2; i<=n+1; i++)
         {
-            a=i-j;
-            b=j;
-
-            if(b>a)
-            {
-                a=a+b;
-                b=a-b;
-                a=a-b;
-            }
-
-            ans+=fact(i,a)/fact(b,1);        
+            curr=first+next;
+            first=next;
+            next=curr;
         }
-        return ans;
+
+        if( n==0 || n==1 )
+            return n;
+        else
+            return curr;
     }
 };
 
@@ -37,8 +27,11 @@ int main()
     cin >> n;
 
     Solution sol;
-    int result=sol.climbStairs(n);
-    cout << result << endl;
+    for( int i=0; i<=n; i++)
+    {
+        int result=sol.climbStairs(i);
+        cout << i << "->" << result << endl;
+    }
 
     return 0;
 }
