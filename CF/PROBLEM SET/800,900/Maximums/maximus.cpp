2cb8377e8bd12e093e8641d8248e528cf69a786d
unknown <raviyaadhav@gmail.com>
2023-08-20 21:20:50 +0530

    Updated Commit

---
+++ CF/PROBLEM SET/800,900/Maximums/maximus.cpp
@@ -0,0 +1,27 @@
+// https://codeforces.com/problemset/problem/1326/B
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+typedef long long int ll;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    ll n;
+    cin >> n;
+
+    ll b, max=0;
+    for( int i=0; i<n; i++)
+    {
+        cin >> b;
+        cout << b+max << " ";
+
+        if( b+max>max )
+            max+=b;
+    }    
+    
+    return 0;
+}
