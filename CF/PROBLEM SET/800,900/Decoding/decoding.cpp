838a16c499467d409881e157bebb21a462537337
unknown <raviyaadhav@gmail.com>
2023-08-23 21:57:19 +0530

    Updated Commit

---
+++ CF/PROBLEM SET/800,900/Decoding/decoding.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h> 
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    int n;
+    string s;
+    cin >> n >> s;
+
+    string ans="";
+
+    for( int i=n-2; i>=0; i-=2)
+        ans+=s[i];
+    for( int i=1-n%2; i<n; i+=2)
+        ans+=s[i];
+
+    cout << ans << endl;
+    
+    return 0;
+}
