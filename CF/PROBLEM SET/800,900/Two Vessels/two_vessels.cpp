c2070715fb777ce9a02ef4e3cbe6f6042f282844
unknown <raviyaadhav@gmail.com>
2023-09-09 22:37:24 +0530

    Updated Commit

---
+++ CF/PROBLEM SET/800,900/Two Vessels/two_vessels.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while( t-- )
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        if(a<b)
+        {
+            a=a+b;
+            b=a-b;
+            a=a-b;
+        }
+
+        cout << (a-b+(2*c)-1)/(2*c) << endl;
+    }
+    
+    return 0;
+}
